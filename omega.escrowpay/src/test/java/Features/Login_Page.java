9487eeb6879f454d19fbeb09ebfe05074a41036d
rakshit <rakshit@amit-IdeaPad-3-15ITL6>
2024-08-27 13:26:48 +0530

    Commit second

--- omega.escrowpay/src/test/java/Features/Login_Page.java
+++ omega.escrowpay/src/test/java/Features/Login_Page.java
@@ -1,10 +1,13 @@
 package Features;
 
+import org.testng.annotations.Test;
+import org.testng.AssertJUnit;
 import java.util.concurrent.TimeUnit;
 
 import org.openqa.selenium.*;
 import org.openqa.selenium.chrome.ChromeDriver;
 import org.openqa.selenium.interactions.Actions;
+import org.openqa.selenium.support.ui.Select;
 import org.testng.Assert;
 import org.testng.annotations.Parameters;
 import org.testng.annotations.Test;
@@ -42,11 +45,11 @@ public class Login_Page {
 		driver.findElement(By.xpath("//*[@id=\"loginForm\"]/div/div[3]/button")).click();
 		String expected_title = "Omega Web | Dashboard";
 		String actual_title = driver.getTitle();
-		Assert.assertEquals(actual_title, expected_title);
+		AssertJUnit.assertEquals(actual_title, expected_title);
 //		driver.close();
 	}
 	@Test
-	public void ProfileDropdown()
+	public void ProfileDropdown() throws InterruptedException
 	{
 		driver.findElement(By.xpath("//div[@class='organization']")).click();
 	}
@@ -90,7 +93,7 @@ public class Login_Page {
 	@Test
 	public void UserManagement()
 	{
-		driver.findElement(By.xpath("//*[@id=\"headinginnerone\"]/a")).click();
+		driver.findElement(By.linkText("User Management")).click();
 	}
 	
 	@Test
@@ -120,28 +123,63 @@ public class Login_Page {
 	@Test
 	public void TransactionReports()
 	{
-		driver.findElement(By.xpath("//*[@id=\"headinginnerone\"]/a")).click();
+		driver.findElement(By.linkText("Transaction Reports")).click();
 	}
 	
 	@Test
 	public void Deals()
 	{
-		driver.findElement(By.xpath("//a[contains(text(),'Deals')]")).click();
+		driver.findElement(By.xpath("//a[normalize-space(text())='Deals']")).click();
 	}
 	
 	@Test
 	public void DealsList()
 	{
-		driver.findElement(By.xpath("//a[ends-with(text(),'List'])")).click();		
+		driver.findElement(By.xpath("//a[normalize-space(text())='Deals List']")).click();		
+	}
+	
+	@Test
+	public void CreateUser()
+	{
+		driver.findElement(By.linkText("Create New User")).click();
+	}
+	
+	@Test
+	public void FillForm()
+	{
+		driver.findElement(By.name("name")).sendKeys("Rakshit_Automated6");
+		driver.findElement(By.name("pan")).sendKeys("DDDTY1234X");
+		driver.findElement(By.name("mobile")).sendKeys("3234467890");
+		driver.findElement(By.name("email")).sendKeys("automated6@gmail.com");
+		driver.findElement(By.xpath("//button[@type='submit']")).click();
+		String expected_url= "https://omega.escrowpayindia.com/admin/users/list";
+		String actual_url = driver.getCurrentUrl();
+		Assert.assertEquals(actual_url, expected_url);
+	}
+	
+	@Test
+	public void CreateDeal()
+	{
+		driver.findElement(By.linkText("Create Deal")).click();
+		driver.findElement(By.xpath("//input[@name='deal_name']")).sendKeys("Test_Deal4");
+		Select dropdown_seller = new Select(driver.findElement(By.id("seller")));
+		dropdown_seller.selectByVisibleText("Rakshit");
+		Select dropdown_buyer = new Select(driver.findElement(By.id("buyer")));
+		dropdown_buyer.selectByVisibleText("Test_Rakshit");
+		driver.findElement(By.xpath("//input[@name='amount']")).sendKeys("1001");
+		driver.findElement(By.xpath("//button[@type='submit']")).click();
+		String expected_url = "https://omega.escrowpayindia.com/admin/deal/list";
+		String actual_url = driver.getCurrentUrl();
+		Assert.assertEquals(actual_url, expected_url);
 	}
 	
 	@Test
 	public void Logout()
 	{
-		driver.findElement(By.xpath("//a[text()='Logout']"));
+		driver.findElement(By.cssSelector("a[onclick*='logoutform']")).click();
 		String expected_title = "Login | Escrowpay";
 		String actual_title = driver.getTitle();
-		Assert.assertEquals(actual_title, expected_title);
+		Assert.assertEquals(actual_title,expected_title);
 	}
 	
 	@Test
